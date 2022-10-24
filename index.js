f5a2fbf3005cfb29bcde53c2b512e2f0eb00fb92
kskanak <kskanak26@gmail.com>
2022-10-25 00:08:04 +0600

    vercel added

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
-const catagories = require("./data/catagory.json");
+const catagories = require("./data/catagories.json");
+const catagorydetails = require("./data/catagorydetails.json");
 
 app.use(cors());
 
@@ -12,6 +13,11 @@ app.get("/", (req, res) => {
 
 app.get("/catagories", (req, res) => {
   res.send(catagories);
+});
+app.get("/catagorydetails/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const details = catagorydetails.find((catagory) => catagory.id === id);
+  res.send(details);
 });
 app.listen(port, () => {
   console.log("api running on port", port);
